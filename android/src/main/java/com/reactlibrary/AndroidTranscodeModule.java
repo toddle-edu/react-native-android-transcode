83fd7952c835e18ab05061470579ee62de8d098b
Karan <karan@Karans-MacBook-Air.local>
2020-03-05 13:47:43 +0530

    Removed override issue

--- android/src/main/java/com/reactlibrary/AndroidTranscodeModule.java
+++ android/src/main/java/com/reactlibrary/AndroidTranscodeModule.java
@@ -35,7 +35,7 @@ public class AndroidTranscodeModule extends ReactContextBaseJavaModule {
         this.reactContext = reactContext;
     }
 
-    @Override
+    //@Override
     public String getName() {
         return "AndroidTranscode";
     }
